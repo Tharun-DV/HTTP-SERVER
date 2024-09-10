a07fb5b4649d12aa59f6ade8fdea4e5067e850b4
Tharun D V <techinformationc@gmail.com>
2024-09-10 18:57:16 +0530

    added index.html response

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::prelude::*;
+use std::fs;
 use std::net::TcpListener;
 use std::net::TcpStream;
 
@@ -6,6 +7,26 @@ fn handle_stream(mut stream: TcpStream) {
     let mut buf = [0; 1024];
     stream.read(&mut buf).expect("Failed to read from stream");
     println!("Received: {}", String::from_utf8_lossy(&buf));
+
+    let get = b"GET / HTTP/1.1\r\n";
+
+    if buf.starts_with(get) {
+
+    let content = fs::read_to_string("src/index.html").expect("Cannot HTML read file");
+
+    let response = format!("HTTP/1.1 200 OK\r\nContent-Length {}\r\n\r\n{}", content.len(),content);
+    stream
+        .write(response.as_bytes())
+        .expect("Cannot write to stream");
+    stream.flush().expect("Cannot flush stream");
+    }else{
+        let content = fs::read_to_string("src/404.html").expect("Cannot HTML read file");
+        let response = format!("HTTP/1.1 404 NOT FOUND\r\nContent-Length {}\r\n\r\n{}",content.len(),content);
+        stream
+            .write(response.as_bytes())
+            .expect("Cannot write to stream");
+        stream.flush().expect("Cannot flush stream");
+    }
 }
 
 fn main() {
