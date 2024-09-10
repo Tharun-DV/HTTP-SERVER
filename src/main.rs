a59ff74c1fa0a5f5559aa25bc371d18f6bd1b4a3
Tharun D V <techinformationc@gmail.com>
2024-09-10 18:36:35 +0530

    Developing a single threaded http server

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,24 @@
-use std::io;
-use std::net::{TcpListener, TcpStream};
+use std::io::prelude::*;
+use std::net::TcpListener;
+use std::net::TcpStream;
 
-/// End User
-struct Clinet;
-
-/// Computer hosting the Web App
-struct Server {
-    connection: TcpListener,
+fn handle_stream(mut stream: TcpStream) {
+    let mut buf = [0; 1024];
+    stream.read(&mut buf).expect("Failed to read from stream");
+    println!("Received: {}", String::from_utf8_lossy(&buf));
 }
-
-impl Server {
-    fn new(address: &str) -> Server {
-        let listner: TcpListener = TcpListener::bind(address).unwrap();
-
-        Server {
-            connection: listner,
-        }
-    }
-}
-
-enum HTTPMethod {
-    GET,
-    POST,
-    PUT,
-    DELETE,
-    PATCH,
-    OPTIONS,
-    HEAD,
-    TRACE,
-    CONNECT,
-}
-///Sent From Client
-struct Request {
-    /// route to the resource
-    resource: String,
-
-    /// HTTP Method like get , post , put , delete and etc...
-    method: HTTPMethod,
-
-    /// headers
-    headers: std::collections::HashMap<String, Vec<String>>,
-
-    /// body
-    body: String,
-}
-impl Request {
-    fn new(stream: TcpStream) -> io::Result<Request> {
-        unimplemented!()
-    }
-}
-
-/// Sent From Server
-struct Response;
 
 fn main() {
-    let server: Server = Server::new("127.0.0.1:8080");
-    for stream in server.connection.incoming().flatten() {
-        let stream = stream.unwrap();
-        println!("Connection established!");
+    let listener = TcpListener::bind("127.0.0.1:8001").expect("could not bind to addresss");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                handle_stream(stream);
+            }
+            Err(e) => {
+                println!("Failed to establish connection: {}", e);
+            }
+        }
     }
 }
